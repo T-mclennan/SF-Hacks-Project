2517eb531e1f9bd7472caaaa436c150c32451ecc
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2021-03-06 01:22:33 -0800

    added contact form template, wired up formspree to send form contents to email. Might update with material UI and Formik later

--- src/pages/browse/BrowsePage.js
+++ src/pages/browse/BrowsePage.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function BrowserPage() {
   return (
-    <div>
+    <section className="browse-page">
       
-    </div>
+    </section>
   )
 }
 
--- src/pages/contact/ContactPage.js
+++ src/pages/contact/ContactPage.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 function ContactPage() {
   return (
-    <div>
-      Contact Page
-    </div>
+    <section className="contact-page">
+      <article className="contact-form">
+        <h3>get in touch</h3>
+        <form action="https://formspree.io/f/xknplldl" method="POST">
+          <div className="form-group">
+            <input type="text" placeholder="name" name="name" className="form-control"/>
+            <input type="email" placeholder="email" name="email" className="form-control"/>
+            <textarea name="message" style={{resize: 'none'}} name="message" placeholder="message" id="" rows="5" className="form-control"/>
+            <button type="submit" className="submit-btn btn">submit here</button>
+          </div>
+        </form>
+      </article>
+    </section>
   )
 }
 
