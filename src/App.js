2166fd5316848a90dd63b7d89060709eeb83265e
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2021-03-05 21:49:33 -0800

    began adding content to landing, started hero and details sections

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import Layout from './components/Layout'
+import { Route, Switch } from 'react-router-dom';
+import Layout from './components/Layout';
+import AboutPage from './pages/about/AboutPage';
+import BrowsePage from './pages/browse/BrowsePage';
+import CreatePage from './pages/create/CreatePage';
+import ContactPage from './pages/contact/ContactPage';
+import Landing from './pages/landing/Landing';
 import './App.css';
 
 function App() {
 
   return (
     <div className="App-component">
-      <Router>
-        <Layout>
-          
-        </Layout>
-      </Router>
+      <Layout>
+        <Switch>
+          <Route exact path='/about' component={AboutPage} />
+          <Route exact path='/browse' component={BrowsePage} />
+          <Route exact path='/create' component={CreatePage} />
+          <Route exact path='/contact' component={ContactPage} />
+          <Route path='/'>
+            <Landing />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
