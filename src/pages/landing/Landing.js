60720aaaf2e8dbe631f989d2a0cacf15f81cb9bc
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2021-04-07 11:30:25 -0700

    ladded another content section to the landing page

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './Hero';
 import Details from './Details';
 import Action from './CallToAction';
+import Wedge from './Wedge'
 
 function Landing() {
   return (
@@ -9,6 +10,7 @@ function Landing() {
       <Hero/>
       <Details />
       <Action />
+      <Wedge />
     </div>
   )
 }
