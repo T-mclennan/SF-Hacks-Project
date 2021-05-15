d17e69c36936dc16f10f09378a4732be493d10bf
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2021-05-14 18:31:42 -0700

    added spinner to Browse and Edit pages

--- src/pages/browse/BrowsePage.js
+++ src/pages/browse/BrowsePage.js
@@ -1,23 +1,27 @@
-import React, {useLayoutEffect, useState} from 'react'
-import {firestore} from '../../firebase/index'
+import React, {useLayoutEffect, useState} from 'react';
+import Loader from 'react-loader-spinner';
+import {firestore} from '../../firebase/index';
 import 'firebase/firestore';
-import PostSample from '../../components/PostSample'
-import '../Pages.css'
+import PostSample from '../../components/PostSample';
+import '../Pages.css';
 
 function BrowsePage() {
-  const [postList, setPostList] = useState([])
+  const [postList, setPostList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const postsRef = firestore.collection('post_content');
 
   const fetchPosts = async () => {
+    setIsLoading(true);
     const data=await postsRef.get();
     data.docs.forEach(item=>{
-      setPostList(oldPosts => [...oldPosts, item.data()])
+      setPostList(oldPosts => [...oldPosts, item.data()]);
      })
+    setIsLoading(false);
   }
 
   useLayoutEffect(() => {
-    fetchPosts()
+    fetchPosts();
   }, []);
 
   return (
@@ -31,6 +35,12 @@ function BrowsePage() {
           return <PostSample key={i} {...post} />
         })}
       </div>
+
+      {isLoading && 
+            <div className="loadingScreen">
+                <Loader type="ThreeDots" color="hsl(205, 84%, 25%)" height={80} width={80} margin="auto"/>
+            </div>
+          }
     </section>
   )
 }
--- src/pages/edit/Edit.js
+++ src/pages/edit/Edit.js
@@ -1,8 +1,10 @@
 import React, {useLayoutEffect, useState} from 'react'
 import { Link, useLocation} from "react-router-dom";
 import {firestore} from '../../firebase/index'
+import Loader from 'react-loader-spinner'
 import 'firebase/firestore';
 import PostForm from '../../components/form/PostForm'
+import Title from "../../components/Title";
 import '../Pages.css'
 
 function useQuery() {
@@ -15,41 +17,52 @@ function Edit() {
     let paramQuery = useQuery();
     const paramID = paramQuery.get("ver")
     const [data, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
   
     //Create firestore query for that ID
     const postsRef = firestore.collection('post_content');
 
     const fetchPosts = async () => {
-      const data=await postsRef.get();
+      setIsLoading(true);
+      const data = await postsRef.get();
       data.docs.forEach(item=>{
         const {vid} = item.data()
         if (vid === `${paramID}`) {
           setData(item.data())
         }
       })
+      setIsLoading(false);
     }
   
     useLayoutEffect(() => {
-      fetchPosts()
+      fetchPosts();
     }, []);
     
+  const editFormContent = 
+    <>
+      <Title title="Edit a Post" mb='1rem'/>
+      <article className="contact-form ">
+        <PostForm 
+          updating={true} 
+          pastValues={data}
+        /> 
+      </article> 
+    </>
+
+  const errorContent = 
+    <article className="contact-form ">
+      <p className="edit-page-header">No posts were found with that ID.</p>
+      <Link to="/" className="btn center-btn">Back Home</Link>
+    </article>
   
   return (
     <section className="create-page">
-        <article className="contact-form ">
-          {
-            data ? <h3>Edit a Post</h3>   
-                 : <>
-                      <p className="edit-page-header">No posts were found with that ID.</p>
-                      <Link to="/" className="btn center-btn">Back Home</Link>
-                   </>
+          {isLoading && 
+            <div className="loadingScreen">
+                <Loader type="ThreeDots" color="hsl(205, 84%, 25%)" height={80} width={80} margin="auto"/>
+            </div>
           }
-
-          {data && <PostForm 
-            updating={true} 
-            pastValues={data}
-          />}
-        </article>
+          {!isLoading && (data ? editFormContent : errorContent)}
     </section>
   )
 }
